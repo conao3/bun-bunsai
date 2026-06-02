8270c093cd3a4a45036f3b12a37bf616a0c6fed0
conao3 <conao3@gmail.com>
2026-06-02 12:46:38 +0900

    add nix flake with bun dev shell

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "bun-bunsai - LocalStack-equivalent AWS API emulator in Bun";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-darwin"
+      ];
+
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          treefmt = {
+            projectRootFile = "flake.nix";
+            programs.prettier.enable = true;
+            programs.nixfmt.enable = true;
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+            ];
+          };
+        };
+    };
+}
